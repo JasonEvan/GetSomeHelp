1d602c356a4b1a4847f9f7b5df90aa3cda42de51
JasonEvan <7ason3van@gmail.com>
2025-12-09 20:51:55 +0700

    chore: define route structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,21 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
 export default function App() {
-  return <div className="text-2xl">Hello World</div>;
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Get Some Help</Link>
+        <Link to="/feature">Feature</Link>
+        <Link to="/career">Career</Link>
+        <Link to="/help">Help</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<></>} />
+        <Route path="/feature" element={<></>} />
+        <Route path="/career" element={<></>} />
+        <Route path="/help" element={<></>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
