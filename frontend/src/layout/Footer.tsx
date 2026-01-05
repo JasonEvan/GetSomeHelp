0ebf44c4b45248d2aead46637382a831663be3c6
JasonEvan <7ason3van@gmail.com>
2026-01-05 15:27:49 +0700

    chore: change tag a to tag Link on Footer

--- frontend/src/layout/Footer.tsx
+++ frontend/src/layout/Footer.tsx
@@ -12,11 +12,7 @@ export default function Footer() {
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div>
             <div className="flex items-center gap-2 mb-3">
-              <img
-                src="/img/icons/logo.png"
-                alt="Logo"
-                className="h-8 w-auto"
-              />
+              <img src="/img/logo.png" alt="Logo" className="h-8 w-auto" />
               <Link to="/" className="text-lg font-semibold">
                 Get Some Help
               </Link>
@@ -31,29 +27,29 @@ export default function Footer() {
             <h4 className="font-semibold mb-4 text-purple-600">Quick Links</h4>
             <ul className="space-y-2 text-sm">
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   Home
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   Popular Services
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   Order
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   Career
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   Help
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -62,24 +58,24 @@ export default function Footer() {
             <h4 className="font-semibold mb-4 text-purple-600">Features</h4>
             <ul className="space-y-2 text-sm">
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   Web-designers
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   Marketers
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   Small Business
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   Website Builder
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -88,50 +84,50 @@ export default function Footer() {
             <h4 className="font-semibold mb-4 text-purple-600">Company</h4>
             <ul className="space-y-2 text-sm">
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   About Us
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   Careers
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   FAQs
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   Teams
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="text-black hover:text-purple-600">
+                <Link to="#" className="text-black hover:text-purple-600">
                   Contact Us
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
         </div>
 
         <div className="mt-10 flex justify-start gap-6 text-black">
-          <a href="#" className="hover:text-purple-600">
+          <Link to="#" className="hover:text-purple-600">
             <FacebookIcon fontSize="small" />
-          </a>
-          <a href="#" className="hover:text-purple-600">
+          </Link>
+          <Link to="#" className="hover:text-purple-600">
             <InstagramIcon fontSize="small" />
-          </a>
-          <a href="#" className="hover:text-purple-600">
+          </Link>
+          <Link to="#" className="hover:text-purple-600">
             <YouTubeIcon fontSize="small" />
-          </a>
-          <a href="#" className="hover:text-purple-600">
+          </Link>
+          <Link to="#" className="hover:text-purple-600">
             <LinkedInIcon fontSize="small" />
-          </a>
-          <a href="#" className="hover:text-purple-600">
+          </Link>
+          <Link to="#" className="hover:text-purple-600">
             <MailOutlineIcon fontSize="small" />
-          </a>
+          </Link>
         </div>
       </div>
     </footer>
